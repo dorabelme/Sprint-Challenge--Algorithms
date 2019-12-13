e65b5b5f45035d3214ab2f70e898fa86afec3f85
Dora Belme <contact.dorabelme@gmail.com>
2019-12-13 13:24:25 -0500

    added robotsort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -97,15 +99,58 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        # pick up the first item
+        self.swap_item()
+
+        # while can move right move right
+        if self.can_move_right():
+            self.move_right()
+
+        # loop till list is sorted
+        while True:
+            print(self._list)
+
+            # if there is no item in the hand of the robot or there is no item at that given position, swap
+            if self.compare_item() == None:
+                self.swap_item()
+
+                # if the robot can move right, move right, otherwise swap
+                if self.can_move_right():
+                    self.move_right()
+                self.swap_item()
+
+            # if you can move right, move right
+            if self.can_move_right():
+                self.move_right()
+
+            # loop for comparing numbers
+            while True:
+                # if the held item's value is greater, swap
+                if self.compare_item() == 1:
+                    self.swap_item()
+                # robot reaches the end of the list and cannot move right, so we break
+                if self.can_move_right() == False:
+                    break
+                self.move_right()
+
+            #  if there is an item in the hand of the robot or there is an item at that given position, move left
+            while self.compare_item() != None:
+                self.move_left()
+
+            # if you cannot move to the right anymore, reached the end of the list, put down the item and break. List is sorted
+            if self.can_move_right() == False:
+                self.swap_item()
+                break
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
+    l = [10, 20, 4, 5]
     robot = SortingRobot(l)
 
     robot.sort()
